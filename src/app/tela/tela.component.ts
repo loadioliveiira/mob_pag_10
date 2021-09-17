9fc7b7925f699982e2598ee3956e48e61d83671e
Igor Lopes <igor.lopes@clinicasim.com>
2021-09-17 10:51:54 -0300

    criacao mobile pagina 10

---
+++ src/app/tela/tela.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela',
+  templateUrl: './tela.component.html',
+  styleUrls: ['./tela.component.scss']
+})
+export class TelaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
